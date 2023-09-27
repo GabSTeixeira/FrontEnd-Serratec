d3a3e906a0a8672793c333a4bc53039d61b8017a
Gabriel <gabrielteixeira.21@hotmail.com>
2023-09-26 23:11:45 -0300

    integração da api com o nintendo ds

--- script/index.js
+++ script/index.js
@@ -1,20 +1,20 @@
 if (localStorage.getItem("acesso") === "true") {
+    //window.open("login.html", "_self")
     window.location.href = "../paginas/ds-fechado.html"
 }
 
 function logar() {
-    const login = document.getElementById("login").value;
+    const email = document.getElementById("email").value;
     const senha = document.getElementById("senha").value;
 
     if(!localStorage.usuario){
-        return alert('Usuario ou senha incorretos!')
+        return alert('Email ou senha incorretos!')
     }
 
     const vetorUsuario = JSON.parse(localStorage.getItem('usuario'));
     for (let usuario of vetorUsuario) {
-        if (usuario.login === login && usuario.senha === senha) {
+        if (usuario.email === email && usuario.senha === senha) {
             localStorage.setItem("acesso", true)
-            alert('Logado com sucesso!');
             return window.location.href = "../index.html";
         }
     }
